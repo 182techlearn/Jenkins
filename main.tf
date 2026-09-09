36172ec7e689c1951c6a67bc63f9d3cd67957c80
182techlearn <99007649+182techlearn@users.noreply.github.com>
2026-09-09 06:28:40 +0530

    Add key_name to AWS instance resource (#5)
    Ok

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "Jenkins_lab" {
   ami           = "ami-081b0a6eac00b4f53" # Amazon Linux 2 AMI (update as needed)
   instance_type = "t3.micro"
+  key_name      = "sshkey-si"
 
   tags = {
     Name = "Linux-jenkins-EC2"
