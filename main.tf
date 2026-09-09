1d55a0cd8841b5c344f518cfc23c0c04f69632b7
182techlearn <99007649+182techlearn@users.noreply.github.com>
2026-09-09 07:07:34 +0530

    Change Terraform state bucket name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "seniyan-terraform-state-bucket"
     key            = "ec2/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
