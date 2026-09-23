428bb97af0eefca9762bc9f33702ce410c9b743e
182techlearn <99007649+182techlearn@users.noreply.github.com>
2026-09-23 21:20:46 +0530

    Change SSH port from 22 to 8089

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_default_security_group" "default" {
   ingress {
     description = "Allow SSH from anywhere"
     from_port   = 22
-    to_port     = 22
+    to_port     = 8089
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]   # replace with your IP for better security
   }
